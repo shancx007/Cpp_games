2874d1b25c02cd9787022cae36fdf3b797dac1d4
shancx007 <shancx007@gmail.com>
2026-04-03 17:01:50 +0530

    Roch paper scissors now

--- R_P_S.cpp
+++ R_P_S.cpp
@@ -1,3 +1,10 @@
 #include<iostream>
 #include<random>
+#include <string>
+using namespace std;
+string choice ={"ROCK","PAPER","SCISSORS"};
+random_device  rd;
+mt19937 gen(rd()); 
+uniform_int_distribution<> rand(0,2);
+
 
