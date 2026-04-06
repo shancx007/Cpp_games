821c5f8545a9bf3f8ac162d6798ad6d38e1a3226
shancx007 <shancx007@gmail.com>
2026-04-06 09:39:03 +0530

    Making a Quiz orianted Game 2

---
+++ Quiz.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <random>
+#include <string>
+using namespace std;
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<> rnd(0,9):
+string q1="Which country is known as the 'Land of the Rising Sun'?";
