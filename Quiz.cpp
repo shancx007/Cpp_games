484c7077a17810961b43dac4830f7f7016c62bb7
shancx007 <shancx007@gmail.com>
2026-04-06 11:20:56 +0530

    Correct syntax and initialize quiz questions
    Fix syntax errors in Quiz.cpp and initialize quiz questions.

--- Quiz.cpp
+++ Quiz.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 random_device rd;
 mt19937 gen(rd());
-uniform_int_distribution<> rnd(0,9):
+uniform_int_distribution<> rnd(0,9);
 struct quiz{
    string q;
    string ans;
     };
-quiz q1={
+quiz q1
+q1.q ={
     {"Which country is known as the 'Land of the Rising Sun'?","Japan"}
     {"What is the hardest natural substance on Earth?","Diamond"}
     };
