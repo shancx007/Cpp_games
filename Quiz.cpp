ef5d1bc517452c0d0a2784a989c5d50acf048ba5
shancx007 <shancx007@gmail.com>
2026-04-07 09:50:47 +0530

    Well i guess we are almost there ^_^

--- Quiz.cpp
+++ Quiz.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
-#include <random>
+#include <algorithm>
 #include <string>
-#include <unordered_map>
+
 using namespace std;
-random_device rd;
-mt19937 gen(rd());
-uniform_int_distribution<> rnd(0,9):
+
 struct quiz{
    string q;
    string ans;
     };
-quiz q1={
-    {"Which country is known as the 'Land of the Rising Sun'?","Japan"}
-    {"What is the hardest natural substance on Earth?","Diamond"}
+quiz ql[10]{
+    {"Which country is known as the 'Land of the Rising Sun'?","Japan"},
+    {"What is the hardest natural substance on Earth?","Diamond"},
+    {"In which year did the Titanic sink on its maiden voyage from Southampton?","1912"},
+    {"Who wrote the famous play Romeo and Juliet?","shakespeare"},
+    {"What is the largest organ in the human body?","skin"},
+    {"Which artist painted the Mona Lisa?","Leanar de vinci"},
+    {"What is the chemical symbol for the element Gold?","Au"},
+    {"Which planet in our solar system is known as the 'Red Planet'","Mars"},
+    {"Which legendary British rock band was fronted by Freddie Mercury?","Queen"},
+    {"What do you call a polygon with eight sides?","Octagon"}
     };
-
-
-unordered_map<string,string> arrang;
-arrang[quiz.q[]]="Japan";
+int main(){
+    shuffle(ql,ql+10,gen);
+    for(int = =0)
+}
