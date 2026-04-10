4aa90aca2575073c8b77174c543a9a12a0addecf
shancx007 <shancx007@gmail.com>
2026-04-10 21:19:04 +0530

    Getting into card flip match 2

--- Card_flip.cpp
+++ Card_flip.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
-#include <unordered_map>
+#include <vector>
 using namespace std;
 random_device rd;
 mt19937 gen(rd());
 string shapes[4]= {"hearts","spade","diamond","leaf"};
-string values[13]={"A","2","3","4","5","6",7","8","9","10","J","Q","K"};
-unordered_map <string,string> cards;
+string values[13]={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+vector <string> cards;
+int main(){
+for (string s : shapes){
+    for(string v : values){
+        cards.push_back( v + "of" + s);
+        
+    }
+}
+    shuffle(cards,cards+52,gen);
+}
