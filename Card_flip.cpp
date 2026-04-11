5f239c8d13cdd401fde4edcf6144ee5bed752b44
shancx007 <shancx007@gmail.com>
2026-04-11 23:01:56 +0530

    I'm trying to create a array to display blank [] so this represents the cards

--- Card_flip.cpp
+++ Card_flip.cpp
@@ -8,6 +8,15 @@ mt19937 gen(rd());
 string shapes[4]= {"hearts","spade","diamond","leaf"};
 string values[13]={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 vector <string> cards;
+vector <string> layout;
+void setup(string temp){
+    cout<<temp[0]<<temp[1]<<temp[2]<<temp[3]<<endl;
+    cout<<temp[4]<<temp[5]<<temp[6]<<temp[7]<<endl;
+    cout<<temp[8]<<temp[9]<<temp[10]<<temp[11]<<endl;
+    cout<<temp[12]<<temp[13]<<temp[14]<<temp[15]<<endl;
+
+}
+//uniform_int_distribution<int> rnd(0,51);
 int main(){
 for (string s : shapes){
     for(string v : values){
@@ -15,5 +24,15 @@ for (string s : shapes){
         
     }
 }
-    shuffle(cards,cards+52,gen);
+    shuffle(cards.begin(),cards.end(),gen);
+    vector <string> board;
+    for(int i; i <= 7;i++){
+        board.push_back(cards[i]);
+        board.push_back(cards[i]);
+        layout.push_back("[ ]")
+
+    }
+    shuffle(board.begin(),board.end(),gen);
+   
+
 }
