0e5352cf3d6add9726cbdaf93f0947cf7dba682f
shancx007 <shancx007@gmail.com>
2026-04-17 21:37:14 +0530

    Added a small screen and to controle the ball or the circle for noe this is done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <raylib.h>
+
+Color mygreen = {30, 160, 120, 255};
+int X_cord = 300;
+int Y_cord = 300;
 int main()
+
+
 {
-    InitWindow(800, 600, "Balls");
+    InitWindow(600, 600, "Balls");
     SetTargetFPS(60);
     while(!WindowShouldClose()){
-        
+        BeginDrawing();
+        ClearBackground(mygreen);
+      
+        DrawCircle(X_cord,Y_cord,10,WHITE);
+        if(IsKeyDown(KEY_RIGHT)) X_cord += 3;
+        if(IsKeyDown(KEY_LEFT))  X_cord -= 3;
+        if(IsKeyDown(KEY_UP)) Y_cord -= 3;
+        if(IsKeyDown(KEY_DOWN)) Y_cord += 3;
+        EndDrawing();
     }
     CloseWindow();
 
