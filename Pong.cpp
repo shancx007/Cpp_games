b8b66e4044ab00541fc302bc0022976d6a9af91a
shancx007 <shancx007@gmail.com>
2026-04-19 16:12:21 +0530

    Well Got it done in one day was a bit tricky but not hard, thanks to gemini and a good teacher from youtude forgot his name will mention next time ^_^

--- Pong.cpp
+++ Pong.cpp
@@ -1,9 +1,7 @@
 #include <raylib.h>
 Color green ={30 , 160 , 120, 255};
 
-float Zx_cords = 30;
-float Zy_cords = 670;
-float Z_radius = 20;
+
 class Ball{
     public: 
         float Y_cord = GetScreenHeight()/2;
@@ -11,7 +9,8 @@ class Ball{
         float radius = 10;
         float x_speed = 6;
         float y_speed = 6;
-        int player1 , player2;
+        int player1 =0, player2 = 0;
+      
       
     void Draw(){
          DrawCircle(X_cord,Y_cord, radius, WHITE);
@@ -26,7 +25,9 @@ class Ball{
 
         }
     int Score(){
-        if (X_cord<=GetScreenWeight())
+       
+        //if (X_cord +radius >= 0 ) player2 += 1
+
     }
     
     
@@ -59,31 +60,61 @@ class Paddle{
 int main(){
    
     InitWindow(1200,680, "Balls");
-
+   
     SetTargetFPS(60);
     Paddle pad;
     Ball ball;
+    bool isGameOver = false;
   
     while (!WindowShouldClose())
     
+    
     {
         BeginDrawing();
-        ClearBackground(green);
-        ball.Draw();
-        pad.Draw();
+        if(isGameOver == false){
+       
 
         //DrawRectangle(10,GetScreenHeight()/2-60,30,120,WHITE);
         //DrawRectangle(GetScreenWidth()-40,GetScreenHeight()/2-60,30,120,WHITE);
-        DrawLine(GetScreenWidth()/2,GetScreenHeight(), GetScreenWidth()/2,0 , WHITE);
-        DrawCircleLines(GetScreenWidth()/2,GetScreenHeight()/2,150,WHITE);
-
+      
         ball.Direction();
         pad.Direction();
         if(CheckCollisionCircleRec({ball.X_cord,ball.Y_cord},ball.radius,{pad.x1,pad.y1,pad.W,pad.H})) ball.x_speed *=-1;
         if(CheckCollisionCircleRec({ball.X_cord,ball.Y_cord},ball.radius,{pad.x2,pad.y2,pad.W,pad.H})) ball.x_speed *=-1;
-        
-       
+        if (ball.X_cord - ball.radius >= GetScreenWidth()){
+            ball.player1+=1;
+            ball.Y_cord = GetScreenHeight()/2;
+            ball.X_cord = GetScreenWidth()/2;
+        }
+        if (ball.X_cord + ball.radius <= 0){
+            ball.player2+=1;
+            ball.Y_cord = GetScreenHeight()/2;
+            ball.X_cord = GetScreenWidth()/2;
+        }
+    }
 
+        if(ball.player1== 10){ 
+            DrawText("PLAYER 1 WINS!!!",300,300,75,RED);
+            isGameOver = true;
+        }
+        if(ball.player2== 10) {
+            
+            DrawText("PLAYER 2 WINS!!!",300,300,75,RED);
+            isGameOver = true;
+
+        }
+    
+        DrawLine(GetScreenWidth()/2,GetScreenHeight(), GetScreenWidth()/2,0 , WHITE);
+        DrawCircleLines(GetScreenWidth()/2,GetScreenHeight()/2,150,WHITE);
+
+
+        DrawText(TextFormat("Score : %d",ball.player1),150,30,75,WHITE);
+        DrawText(TextFormat("Score : %d",ball.player2),750,30,75,BLUE);
+         ClearBackground(green);
+        ball.Draw();
+        pad.Draw();
+       
+    
 
         EndDrawing();
     }
