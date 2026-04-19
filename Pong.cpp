bd9638b54461853ffc060bd6211205a3d8090f4d
shancx007 <shancx007@gmail.com>
2026-04-19 19:57:03 +0530

    Well Got it done in one day was a bit tricky but not hard, thanks to gemini and a good teacher from youtude forgot his name will mention next time ^_^ 2

--- Pong.cpp
+++ Pong.cpp
@@ -24,33 +24,29 @@ class Ball{
        
 
         }
-    int Score(){
-       
-        //if (X_cord +radius >= 0 ) player2 += 1
-
-    }
+ 
     
     
 };
 class Paddle{
     public:
         float x1 = 10;
-        float y1 = GetScreenHeight()/2-60;
+        float y1 = GetScreenHeight()/2-70;
         float x2 = GetScreenWidth()-30;
-        float y2 = GetScreenHeight()/2-60;
-        float H = 120;
+        float y2 = GetScreenHeight()/2-70;
+        float H = 140;
         float W = 20;
     void Draw(){
        DrawRectangle(x1,y1,W,H,WHITE);
        DrawRectangle(x2,y2,W,H,WHITE);
     }
      void Direction(){
-        if(IsKeyDown(KEY_UP)) y2 -= 7;
-        if(IsKeyDown(KEY_DOWN)) y2 += 7;
+        if(IsKeyDown(KEY_UP)) y2 -= 8;
+        if(IsKeyDown(KEY_DOWN)) y2 += 8;
         if(y2<=5)y2=5;
         if(y2>= GetScreenHeight()-130)y2= GetScreenHeight()-130;
-        if(IsKeyDown(KEY_W)) y1 -= 7;
-        if(IsKeyDown(KEY_S)) y1 += 7;
+        if(IsKeyDown(KEY_W)) y1 -= 8;
+        if(IsKeyDown(KEY_S)) y1 += 8;
         if(y1<=5)y1=5;
         if(y1 >= GetScreenHeight()-130)y1= GetScreenHeight()-130;
      }
@@ -64,23 +60,22 @@ int main(){
     SetTargetFPS(60);
     Paddle pad;
     Ball ball;
+    
     bool isGameOver = false;
-  
     while (!WindowShouldClose())
     
     
     {
         BeginDrawing();
+        ClearBackground(green);
         if(isGameOver == false){
        
-
-        //DrawRectangle(10,GetScreenHeight()/2-60,30,120,WHITE);
-        //DrawRectangle(GetScreenWidth()-40,GetScreenHeight()/2-60,30,120,WHITE);
-      
         ball.Direction();
         pad.Direction();
-        if(CheckCollisionCircleRec({ball.X_cord,ball.Y_cord},ball.radius,{pad.x1,pad.y1,pad.W,pad.H})) ball.x_speed *=-1;
-        if(CheckCollisionCircleRec({ball.X_cord,ball.Y_cord},ball.radius,{pad.x2,pad.y2,pad.W,pad.H})) ball.x_speed *=-1;
+        ball.Draw();
+        pad.Draw();
+       
+        
         if (ball.X_cord - ball.radius >= GetScreenWidth()){
             ball.player1+=1;
             ball.Y_cord = GetScreenHeight()/2;
@@ -91,6 +86,9 @@ int main(){
             ball.Y_cord = GetScreenHeight()/2;
             ball.X_cord = GetScreenWidth()/2;
         }
+        if(CheckCollisionCircleRec({ball.X_cord,ball.Y_cord},ball.radius,{pad.x1,pad.y1,pad.W,pad.H})) ball.x_speed *=-1;
+        if(CheckCollisionCircleRec({ball.X_cord,ball.Y_cord},ball.radius,{pad.x2,pad.y2,pad.W,pad.H})) ball.x_speed *=-1;
+       
     }
 
         if(ball.player1== 10){ 
@@ -110,9 +108,8 @@ int main(){
 
         DrawText(TextFormat("Score : %d",ball.player1),150,30,75,WHITE);
         DrawText(TextFormat("Score : %d",ball.player2),750,30,75,BLUE);
-         ClearBackground(green);
-        ball.Draw();
-        pad.Draw();
+      
+       
        
     
 
