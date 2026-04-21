5c0025426883794600e936b9e25feb3c32fbfda6
shancx007 <shancx007@gmail.com>
2026-04-21 09:16:19 +0530

    A new game the classic Snake Let's see how long this takes i am thinking of cracking this on my own with out any help so might take a while, Adios ^_^

---
+++ Snake.cpp
@@ -0,0 +1,11 @@
+#include <raylib.h>
+
+int main(){
+    InitWindow(1200,700,Snakes);
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        
+        EndDrawing();
+    }
+    CloseWindow();
+}
