813981b225b06e083702ff5355ab33482c1192f9
shancx007 <shancx007@gmail.com>
2026-04-24 19:30:29 +0530

    Wel this is a disaster i am stuck i don't have my phone so i can't really figure out how to Make the snake move in the snake game way i am trying i have a cupple ideas tho but since i broke my phone i don't have internet

--- Snake.cpp
+++ Snake.cpp
@@ -1,9 +1,35 @@
 #include <raylib.h>
+Color mygreen = {30, 160, 120, 255};
+
+
+class Head{
+    public:
+    int x = 600;
+    int y = 350;
+    void Draw(){
+        DrawRectangle(x,y,20,20,WHITE);
+    }
+   
+        
+        
+
+};
 
 int main(){
-    InitWindow(1200,700,Snakes);
+    Head head;
+    InitWindow(1200,700,"Snakes");
+   
+    SetTargetFPS(60);
     while(!WindowShouldClose()){
+        ClearBackground(mygreen);
         BeginDrawing();
+        head.Draw();
+        if(IsKeyPressed(KEY_UP)){
+            while(true){
+                head.x+=3;
+            }
+        }
+        
         
         EndDrawing();
     }
