698569cc11788c38595fd006e42673f6ba7be939
shancx007 <shancx007@gmail.com>
2026-04-29 22:03:47 +0530

    I am now making a Tetirs and i know The snake game has bugs but i am into this right now i juat wanna make somthing new with the things i figured2

--- Tetris.cpp
+++ Tetris.cpp
@@ -2,12 +2,18 @@
 float cellsize = 30;
 float cellcount = 25;
 Color violet = {5,155,200,255};
+class Square{
+    void place(){
+        
+    }
+};
 
 int main(){
     InitWindow(cellsize*cellcount,cellsize*cellcount,"TETRIS");
     while (!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(violet);
+        
 
         EndDrawing();
     }
