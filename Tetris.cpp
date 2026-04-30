f82b1e272719cf9dbb51acb84520e1e6e6c4d0ee
shancx007 <shancx007@gmail.com>
2026-04-30 19:10:41 +0530

    I am stuck burh

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,21 +1,22 @@
 #include <raylib.h>
 #include <deque>
+using namespace std;
 float cellsize = 30;
 float cellcount = 25;
 Color violet = {5,155,200,255};
-float x = 5;
-float y = 5;
+
 class Moids{
+    float x = 5;
+    float y = 5;
     public:
-        
+    deque<Vector2> blocks{
         deque<Vector2> Long={Vector2{x,y},Vector2{x,y},Vector2{x,y},Vector2{x,y}};
-        deque<Vector2> Z_1 = {Vector2{x,y},Vector2{x+1,y},Vector2{x+1,y-1},Vector2{x+1,y-2}};
-        deque<Vector2> Z_2 = {Vector2{x,y},Vector2{x,y+1},Vector2{x+1,y+1},Vector2{x+1,y+2}};
-        deque<Vector2> B_l={Vector2{x,y},Vector2{x+1,y},Vector2{x+1,y+1},Vector2{x+1,y+2}};
-        deque<Vector2> B_r={Vector2{x,y},Vector2{x,y+1},Vector2{x,y+2},Vector2{x+1,y}};
-        deque<Vector2> Iso={Vector2{x,y},Vector2{x,y+1},Vector2{x+1,y},Vector2{x+1,y+1}};
-        deque<Vector2> Square={Vector2{x,y},Vector2{x+1,y},Vector2{x+1,y+1},Vector2{x,y+1}};
-        
+        deque<Vector2> Z_1 = {Vector2{x,y},Vector2{x+1,y},Vector2{x+1,y-1},Vector2{x+1,y-2}},
+        deque<Vector2> Z_2 = {Vector2{x,y},Vector2{x,y+1},Vector2{x+1,y+1},Vector2{x+1,y+2}},
+        deque<Vector2> B_l={Vector2{x,y},Vector2{x+1,y},Vector2{x+1,y+1},Vector2{x+1,y+2}},
+        deque<Vector2> B_r={Vector2{x,y},Vector2{x,y+1},Vector2{x,y+2},Vector2{x+1,y}},
+        deque<Vector2> Iso={Vector2{x,y},Vector2{x,y+1},Vector2{x+1,y},Vector2{x+1,y+1}},
+        deque<Vector2> Square={Vector2{x,y},Vector2{x+1,y},Vector2{x+1,y+1},Vector2{x,y+1}},};
         void Draw(){
             DrawRectangle(x*cellsize,y*cellsize,cellsize,cellsize,BLACK);
         }
