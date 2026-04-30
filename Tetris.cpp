3b96d6cdbcdbcc9c682f0d23446fab110ef5ceb5
shancx007 <shancx007@gmail.com>
2026-04-30 13:48:08 +0530

    I am now making a Tetirs and i know The snake game has bugs but i am into this right now i juat wanna make somthing new with the things i figured3

--- Tetris.cpp
+++ Tetris.cpp
@@ -3,8 +3,11 @@ float cellsize = 30;
 float cellcount = 25;
 Color violet = {5,155,200,255};
 class Square{
-    void place(){
-        
+    void shape(){
+        Vector2 Long{{x,y},{x,y},{x,y},{x,y}};
+        vector2 Z_1 = {{x,y},{x+1,y},{x+1,y-1},{x+1,y-2}};
+        Vector2 Z_2 = {{x,y},{x,y+1},{x+1,y+1},{x+1,y+2}};
+        Vector2 B_l{{x,y},{x+1,y},{x+1,y+1},{x+1,y+2}};
     }
 };
 
